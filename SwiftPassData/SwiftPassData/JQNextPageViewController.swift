99a49c7d82b58efc71f7c5795641407a07885bac
Jacuqueline_Hsu <hsufuchiung@gmail.com>
2019-03-25 23:09:37 +0800

    Save

--- SwiftPassData/SwiftPassData/JQNextPageViewController.swift
+++ SwiftPassData/SwiftPassData/JQNextPageViewController.swift
@@ -8,29 +8,63 @@
 
 import UIKit
 
-class JQNextPageViewController: UIViewController {
+//vc2 to vc1 delegate
+protocol secondPageTextDelegate: AnyObject {
+    func getSecondText(_ vc: JQNextPageViewController, didGet value: String)
+}
 
+class JQNextPageViewController: UIViewController{
+    
     @IBOutlet weak var nextPageTextField: UITextField!
     
-    @IBOutlet weak var nextPageText: UILabel!
+    @IBOutlet weak var pageInfoText: UILabel!
     
+    //vc2 to vc1 delegate
+//    weak var delegate: secondPageTextDelegate?
     
-    //1. Property
+    //vc1 to vc2 Property
     var textInfo: String?
+    
+    //vc2 to vc1 KVO
+    @objc var secondTextInfo = SecondTextInfo()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //1. Property
-        nextPageText.text = textInfo
+        //vc1 to vc2 Property
+//        nextPageText.text = textInfo
+        
+        
     }
     
 
     @IBAction func backBtnAction(_ sender: Any) {
+
         
+        guard let textInfo = nextPageTextField.text else { return }
+        print(textInfo)
+        
+        //vc2 to vc1 delegate
+//        self.delegate?.getSecondText(self, didGet: textInfo)
+        
+        //vc2 to vc1 notification
+//        notificationSecondPage(text: textInfo)
+        
+        //vc2 to vc1 KVO
+        secondTextInfo.textInfo = textInfo
+        
+        //
         navigationController?.popViewController(animated: true)
-        
     }
 
-
+    //vc2 to vc1 notification
+//    func notificationSecondPage(text: String){
+//        NotificationCenter.default.post(name: Notification.Name("secondPageInfo"), object: nil, userInfo: ["text": text])
+//    }
+    
+    //vc2 to vc1 notification
+//    @objc func getTextInfo(data: Notification) {
+//        pageInfoText.text = data.userInfo?["text"] as? String
+//    }
+    
 }
--- SwiftPassData/SwiftPassData/JacquelineViewController.swift
+++ SwiftPassData/SwiftPassData/JacquelineViewController.swift
@@ -8,41 +8,111 @@
 
 import UIKit
 
-//KVO
-class TextInfo: NSObject {
+//vc1 to vc2 KVO
+class FirstTextInfo: NSObject {
     @objc dynamic var textInfo = String()
+    
+    override init() {
+        self.textInfo = " "
+        super.init()
+    }
 }
 
-//delegate
+//vc2 to vc1 KVO
+class SecondTextInfo: NSObject {
+    @objc dynamic var textInfo = String()
+    
+    override init() {
+        self.textInfo = " "
+        super.init()
+    }
+}
 
-class JacquelineViewController: UIViewController {
 
+class JacquelineViewController: UIViewController
+// ,secondPageTextDelegate
+{
+    
     @IBOutlet weak var firstTextField: UITextField!
     
-    @IBOutlet weak var fitstPageText: UILabel!
+    @IBOutlet weak var pageInfoText: UILabel!
     
-    //1. Property
-    var nextPageTextInfo: String?
-
-    //KVO
+    var secondText: String?
+    
+    //vc1 to vc2 KVO
+    @objc var firstTextInfo = FirstTextInfo()
+    
+    //vc2 to vc1 KVO
+    @objc var secondTextInfo = SecondTextInfo()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //vc2 to vc1 notification
+//        NotificationCenter.default.addObserver(self, selector: #selector(getTextInfo(data:)), name: NSNotification.Name("secondPageInfo") , object: nil)
+        
+        //vc2 to vc1 KVO
+        addObserver(self, forKeyPath: #keyPath(secondTextInfo.textInfo), options: .new, context: nil)
+    
     }
     
-    //1. Property
+    //vc2 to vc1 notification
+//    @objc func getTextInfo(data: Notification) {
+//        pageInfoText.text = data.userInfo?["text"] as? String
+//        print("-----notification-----")
+//    }
+//
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "nextPageSegue" {
-            if let vc = segue.destination as? JQNextPageViewController {
-                vc.textInfo = firstTextField.text
-            }
+            let vc = segue.destination as! JQNextPageViewController
+            vc.loadViewIfNeeded()
+            
+            //vc1 to vc2 Property
+//            vc.textInfo = firstTextField.text
+            
+            //vc2 to vc1 delegate
+//            vc.delegate = self
+            
+            //vc1 to vc2 notificaiton
+//            NotificationCenter.default.addObserver(vc, selector: #selector(getTextInfo(data:)), name: NSNotification.Name(rawValue: "firstPageInfo"), object: nil)
         }
-    }
-    
-    @IBAction func nextPage(_ sender: Any) {
-    //1. Property
-       self.performSegue(withIdentifier: "nextPageSegue", sender: nil)
 
     }
+    
+    //vc2 to vc1 delegate
+//    func getSecondText(_ vc: JQNextPageViewController, didGet value: String) {
+//        secondText = value
+//    }
+    
+    //vc2 to vc1 delegate
+//    override func viewWillAppear(_ animated: Bool) {
+//        guard  secondText != nil else { return }
+//        firstPageText.text = secondText
+//    }
+
+    //vc1 to vc2 notification
+//    func notificationFirstPage(text: String) {
+//        NotificationCenter.default.post(name: Notification.Name("firstPageInfo"), object: nil, userInfo: ["text": text])
+//    }
+    
+    
+    @IBAction func nextPage(_ sender: Any) {
+        self.performSegue(withIdentifier: "nextPageSegue", sender: nil)
+        
+        //vc1 to vc2 notification
+//        guard let textInfo = firstTextField.text else { return }
+//        notificationFirstPage(text: textInfo)
+        
+    }
+    
+    //vc2 to vc1 KVO
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == #keyPath(secondTextInfo.textInfo) {
+            guard let updateText = change?[.newKey] as? String else { return }
+            pageInfoText.text = updateText 
+        }
+    }
 }
+
+
