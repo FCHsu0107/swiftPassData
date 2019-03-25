467e3fcb0f3659a79b4cbaa88adafe40bf9bec0a
Jacuqueline_Hsu <hsufuchiung@gmail.com>
2019-03-25 13:52:15 +0800

    Create the files for Jacqueline assignmnent and Set the layout of the project

---
+++ SwiftPassData/SwiftPassData/JacquelineViewController.swift
@@ -0,0 +1,30 @@
+//
+//  JacquelineViewController.swift
+//  SwiftPassData
+//
+//  Created by Fu-Chiung HSU on 2019/3/25.
+//  Copyright © 2019 HuangYenHan. All rights reserved.
+//
+
+import UIKit
+
+class JacquelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
