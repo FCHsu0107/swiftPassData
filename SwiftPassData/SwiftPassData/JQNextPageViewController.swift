028ca1051a159b9d70d264d1dfd09cf9617763c8
Jacuqueline_Hsu <hsufuchiung@gmail.com>
2019-03-25 13:53:56 +0800

    Set the environment for JQ assignment

--- SwiftPassData/SwiftPassData/JQNextPageViewController.swift
+++ SwiftPassData/SwiftPassData/JQNextPageViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class JQNextPageViewController: UIViewController {
 
+    @IBOutlet weak var nextPageTextField: UITextField!
+    
+    @IBOutlet weak var nextPageText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +21,9 @@ class JQNextPageViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backBtnAction(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
     }
-    */
+
 
 }
--- SwiftPassData/SwiftPassData/JacquelineViewController.swift
+++ SwiftPassData/SwiftPassData/JacquelineViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class JacquelineViewController: UIViewController {
 
+    @IBOutlet weak var firstTextField: UITextField!
+    
+    @IBOutlet weak var fitstPageText: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +22,5 @@ class JacquelineViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
